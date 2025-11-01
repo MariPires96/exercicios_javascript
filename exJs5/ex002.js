d2d8e5f282a9a5f45a2d5f3c1883eed55db7d748
MariPires96 <mariana.pires96@hotmail.com>
2025-11-01 09:11:55 -0300

    Conclusão do exercício 2

--- exJs5/ex002.js
+++ exJs5/ex002.js
@@ -3,4 +3,24 @@
 import readline from 'readline-sync'
 
 let numero;
+let parOuImpar;
+let positivoOuNegativo;
 
+numero = readline.questionInt("Digite um número: ");
+
+if (numero % 2 === 0) {
+    parOuImpar = "par";
+} else {
+    parOuImpar = "ímpar";
+}
+
+if (numero > 0) {
+    positivoOuNegativo = "positivo";
+} else if (numero < 0) {
+    positivoOuNegativo = "negativo";
+} else {
+    // Caso especial: o número é zero
+    positivoOuNegativo = "neutro (zero)";
+}
+
+console.log(`O Número ${numero} é ${parOuImpar} e ${positivoOuNegativo}!`);
